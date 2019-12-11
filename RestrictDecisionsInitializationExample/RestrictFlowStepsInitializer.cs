1e3c994328861e84bf76a4212c4ceb4acf75e972
jason-decisions <jason@decisions.com>
2019-12-11 16:42:21 -0500

    Uploading SDK example which filters certain steps out of the step catalog using an initializer, Folder Behavior, and Flow Behavior.

---
+++ RestrictDecisionsInitializationExample/RestrictFlowStepsInitializer.cs
@@ -0,0 +1,33 @@
+﻿using DecisionsFramework.ServiceLayer;
+using DecisionsFramework.ServiceLayer.Services.Folder;
+using DecisionsFramework.ServiceLayer.Utilities;
+
+namespace RestrictedDecisionsInitializationExample
+{
+    /*
+     * This class is implementing the IInitializable interface in order to Initialize the locals module.
+     */
+    public class RestrictFlowStepsInitializer : IInitializable
+    {
+        internal const string RESTRICTED_STEPS_FOLDER_EXAMPLE = "RESTRICTED.FLOW.STEPS";
+
+        //This method is called when the module is initialized
+        public void Initialize()
+        {
+            //Create the Base Flow Folder which will take on the implemented behavior if it doesn't already exist
+            //This folder will be added for everyone
+            SystemUserContext suc = new SystemUserContext();
+            if (FolderService.Instance.Exists(suc, RESTRICTED_STEPS_FOLDER_EXAMPLE) == false)
+            {
+                //Create the folder at the root
+                FolderService.Instance.CreateRootFolder(suc, RESTRICTED_STEPS_FOLDER_EXAMPLE, "Restricted Flow Steps", typeof(RestrictedStepsFolderBehavior).FullName);
+
+                //Add all Permissions besides ADMIN for the DESIGNERS group so DESIGNERS can create flows in this folder.
+                FolderService.Instance.AddGroupPermission(UserContextHolder.GetCurrent(), RESTRICTED_STEPS_FOLDER_EXAMPLE, Constants.DESIGNERS_GROUP_ID, FolderPermission.AllButAdmin);
+            }
+
+            //Example Call to REMOVE the created folder
+            //FolderService.Instance.DeleteFolder(suc, RESTRICTED_STEPS_FOLDER_EXAMPLE, false);
+        }
+    }
+}
---
+++ RestrictDecisionsInitializationExample/RestrictedStepsFlowBehavior.cs
@@ -0,0 +1,37 @@
+﻿using DecisionsFramework.Design.Flow;
+using System.Linq;
+
+namespace RestrictedDecisionsInitializationExample
+{
+    /*
+    * This class is extended the DefaultFlowBehavior to inherit most behavior from the Default Flow Behavior
+    * and just override what we need to override for a specific use case.
+    */
+    class RestrictedStepsFlowBehavior : DefaultFlowBehavior
+    {
+        //Override so that the user cannot change the Behavior Type of Flows with this Behavior
+        public override bool IsUserSettable
+        {
+            get { return false; }
+        }
+
+        //Override to filter certain Steps out of the Step Toolbox
+        public override bool ToolboxFilterOut(Flow f, string[] category, string name, bool isCategory)
+        {
+            //If the category array contains .Net Libraries, return true to filter it out
+            if (category != null && category.FirstOrDefault(c => c.Contains(".Net Libraries")) != null)
+            {
+                return true;
+            }
+
+            //If the name contains .Net Libraries, return true to filter it out (This will remove the 
+            //category itself)
+            if (!string.IsNullOrEmpty(name) && name.Equals(".Net Libraries"))
+            {
+                return true;
+            }
+
+            return base.ToolboxFilterOut(f, category, name, isCategory);
+        }
+    }
+}
