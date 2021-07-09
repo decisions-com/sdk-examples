7a0bd3e3ffd0662a20515d459a528c684707ab14
Khuzema <khuzema@decisions.com>
2021-07-09 22:04:46 +0530

    remove removed output and binutils path

--- Decisions.CustomLogin.Example/Decisions.CustomLogin.Web/Controllers/CustomLoginController.cs
+++ Decisions.CustomLogin.Example/Decisions.CustomLogin.Web/Controllers/CustomLoginController.cs
@@ -1,17 +1,14 @@
-﻿using Decisions.Web.Core.Models;
-using Decisions.Web.Core.Models.Account;
+﻿using Decisions.Web.Core.CustomLogin;
+using Decisions.Web.Core.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 
 namespace Decisions.UnitTests.Web.Core.Controllers
 {
     public class CustomLoginController : Controller, ICustomLoginPageProvider
     {
+        public string LoginLayoutPath { get => "~/VIEWS/CUSTOMLOGIN/CUSTOMLOGINLAYOUT.CSHTML"; set { } }
+
         [AllowAnonymous]
         public IActionResult RedirectToCustomLogin(LoginViewModel loginViewModel)
         {
