a0cd6dac626d5791bea18c1cef548a513a2ba9ff
jason@decisions.com <jason@decisions.com>
2022-08-11 13:55:20 -0400

    Restructuring example so it can be built successfully.

--- Decisions.CustomLogin.Example/BuildModule.ps1
+++ Decisions.CustomLogin.Example/BuildModule.ps1
@@ -6,6 +6,23 @@ param (
 )
 
 function FindMSBuild {
+    $guess = Join-Path -Path ${Env:ProgramFiles(x86)} -ChildPath "Microsoft Visual Studio\2022\BuildTools\MSBuild\Current\Bin\msbuild.exe"
+    if (Test-Path -PathType leaf -LiteralPath $guess ) {
+        return $guess
+    }
+
+    # Check for Visual Studio MSBuild version 2019 (15.0)
+    $guess = Join-Path -Path ${Env:ProgramFiles(x86)} -ChildPath "Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\bin\msbuild.exe"
+    if (Test-Path -PathType leaf -LiteralPath $guess ) {
+        return $guess
+    }
+
+    # Check for Visual Studio MSBuild version 2019 (15.0)
+    $guess = Join-Path -Path ${Env:ProgramFiles(x86)} -ChildPath "Microsoft Visual Studio\2022\Professional\MSBuild\Current\bin\msbuild.exe"
+        if (Test-Path -PathType leaf -LiteralPath $guess ) {
+        return $guess
+    }
+	
     $guess = Join-Path -Path ${Env:ProgramFiles(x86)} -ChildPath "Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\msbuild.exe"
     if (Test-Path -PathType leaf -LiteralPath $guess ) {
         return $guess
@@ -57,8 +74,57 @@ function GetCompileTarget($basePath) {
     throw "Could not find a build.proj file, please create one."
 }
 
+function StopDecisionsServer {
+    $local:service = (get-service "DecisionsServer");
+
+    if ($local:service.status -eq "Running") {
+        $local:service.Stop()
+    }
+
+    Write-Output "stopping Decisions Server..."
+    do { $local:service.refresh(); sleep 1; } until ($local:service.status -eq "Stopped")
+}
+
+function StartDecisionsServer {
+    $local:service = (get-service "DecisionsServer");
+    Write-Output "starting Decisions Server..."
+    $local:service.Start()
+
+    do { $local:service.refresh(); sleep 1; } until ($local:service.status -eq "Running")
+    Write-Output "SHM Started."
+}
+
+function FindModuleName($buildProj)
+{
+    [xml]$local:XmlDocument = Get-Content -Path $buildProj
+
+    foreach($local:target in $local:XmlDocument.Project.Target){
+        if($local:target.Name -eq "build_module")
+        {
+            $local:cmdline = $local:target.Exec.Command
+            break
+        }
+    }
+
+    $local:cmdline = $local:cmdline -split ' '
+    $local:index = [array]::indexof($local:cmdline,"-buildmodule")
+    $local:index++
+
+    return $local:cmdline[$local:index]
+}
+
+function CopyModule($basePath)
+{
+    $local:moduleName = FindModuleName("$basePath\build.proj")
+    $local:fullModuleName = "$basePath\$local:moduleName.zip"
+    $local:destination  = "C:\Program Files\Decisions\Decisions Server\CustomModules\$local:moduleName.zip"
+
+    Write-Output "Copying module..."
+    Copy-Item $local:fullModuleName $local:destination
+}
+
 function FindSolutionFile($basePath) {
-    $local:filelist = Get-ChildItem -Path "$basePath\Decisions.CustomLogin.Web\*.sln"
+    $local:filelist = Get-ChildItem -Path "$basePath\*.sln"
     if($local:filelist.Length -eq 0)
     {
         throw "Can not find *.sln file"
@@ -87,3 +153,7 @@ if ($LastExitCode -ne 0)
 {
    throw "Compile failed with the return code: $LastExitCode"
 }
+
+StopDecisionsServer
+CopyModule($basePath)
+StartDecisionsServer
