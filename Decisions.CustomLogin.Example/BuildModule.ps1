5371b2cb61869cf7328204a823d55215365760dd
Khuzema <khuzema@decisions.com>
2023-08-17 21:51:42 +0530

    updating to .net7 using dot-net instead of msbuild updating login.cshtml to have eye to show password

--- Decisions.CustomLogin.Example/BuildModule.ps1
+++ Decisions.CustomLogin.Example/BuildModule.ps1
@@ -5,67 +5,6 @@ param (
     [Parameter(Mandatory=$false)][string]$framework
 )
 
-function FindMSBuild {
-    $guess = Join-Path -Path ${Env:ProgramFiles(x86)} -ChildPath "Microsoft Visual Studio\2022\BuildTools\MSBuild\Current\Bin\msbuild.exe"
-    if (Test-Path -PathType leaf -LiteralPath $guess ) {
-        return $guess
-    }
-
-    # Check for Visual Studio MSBuild version 2019 (15.0)
-    $guess = Join-Path -Path ${Env:ProgramFiles(x86)} -ChildPath "Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\bin\msbuild.exe"
-    if (Test-Path -PathType leaf -LiteralPath $guess ) {
-        return $guess
-    }
-
-    # Check for Visual Studio MSBuild version 2019 (15.0)
-    $guess = Join-Path -Path ${Env:ProgramFiles(x86)} -ChildPath "Microsoft Visual Studio\2022\Professional\MSBuild\Current\bin\msbuild.exe"
-        if (Test-Path -PathType leaf -LiteralPath $guess ) {
-        return $guess
-    }
-	
-    $guess = Join-Path -Path ${Env:ProgramFiles(x86)} -ChildPath "Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\msbuild.exe"
-    if (Test-Path -PathType leaf -LiteralPath $guess ) {
-        return $guess
-    }
-
-    # Check for Visual Studio MSBuild version 2019 (15.0)
-    $guess = Join-Path -Path ${Env:ProgramFiles(x86)} -ChildPath "Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\bin\msbuild.exe"
-    if (Test-Path -PathType leaf -LiteralPath $guess ) {
-        return $guess
-    }
-
-    # Check for Visual Studio MSBuild version 2019 (15.0)
-    $guess = Join-Path -Path ${Env:ProgramFiles(x86)} -ChildPath "Microsoft Visual Studio\2019\Professional\MSBuild\Current\bin\msbuild.exe"
-        if (Test-Path -PathType leaf -LiteralPath $guess ) {
-        return $guess
-    }
-
-    $guess = Join-Path -Path ${Env:ProgramFiles(x86)} -ChildPath "\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\msbuild.exe"
-    if (Test-Path -PathType leaf -LiteralPath $guess ) {
-        return $guess
-    }
-
-    $guess = Join-Path -Path ${Env:ProgramFiles(x86)} -ChildPath "\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\bin\msbuild.exe"
-    if (Test-Path -PathType leaf -LiteralPath $guess ) {
-        return $guess
-    }
-
-    $guess = Join-Path -Path ${Env:ProgramFiles(x86)} -ChildPath "\Microsoft Visual Studio\2019\Professional\MSBuild\Current\bin\msbuild.exe"
-    if (Test-Path -PathType leaf -LiteralPath $guess ) {
-        return $guess
-    }
-
-    $guess = Join-Path -Path ${Env:ProgramFiles(x86)} -ChildPath "\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\bin\msbuild.exe"
-    if (Test-Path -PathType leaf -LiteralPath $guess ) {
-        return $guess
-    }
-
-    $guess = Join-Path -Path ${Env:ProgramFiles(x86)} -ChildPath "\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\msbuild.exe"
-    if (Test-Path -PathType leaf -LiteralPath $guess ) {
-        return $guess
-    }
-}
-
 function GetCompileTarget($basePath) {
     $guess = Join-Path -Path $basePath -ChildPath "build.proj"
     if (Test-Path -PathType leaf -LiteralPath $guess ) {
@@ -132,14 +71,6 @@ function FindSolutionFile($basePath) {
     return $local:filelist[0].FullName
 }
 
-if ($msbuild) {
-    Write-Output "Using $msbuild"
-} else {
-    Write-Output "Looking for MSBUILD.exe"
-    $msbuild = FindMSBuild
-    Write-Output "Found and Trying $msbuild"
-}
-
 $basePath = (Get-Location).Path
 Write-Output "Using basePath = $basePath"
 
@@ -147,8 +78,8 @@ Write-Output "Compiling Project by build.proj, or by .sln file."
 $compiletarget = GetCompileTarget $basepath
 
 $solution = FindSolutionFile($basePath)
-& $msbuild -t:restore $solution
-& $msbuild $compiletarget
+& dotnet build -t:restore $solution
+& dotnet build $compiletarget
 if ($LastExitCode -ne 0)
 {
    throw "Compile failed with the return code: $LastExitCode"
